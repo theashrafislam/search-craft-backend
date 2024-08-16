1e199bd75405f7309b9a6d86b1b6a43e880864fa
Ashraful Islam <theashrafislam@gmail.com>
2024-08-16 16:28:12 +0600

    all backend setup done

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
+require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 //middlewere
 app.use(cors());
 app.use(express.json());
 
+console.log(process.env.DB_USER);
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<username>:<password>@cluster0.gphdl2n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gphdl2n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,11 +28,13 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const information = client.db('search-craft').collection('information')
 
 
-
-
-  
+    app.get('/information', async (req, res) => {
+      const data = await information.find().toArray();
+      res.send(data);
+    })
 
 
 
