0bd1b5929b841f021b363b74719972d49eb8ca7c
Ashraful Islam <theashrafislam@gmail.com>
2024-08-17 12:12:35 +0600

    updated

--- api/index.js
+++
@@ -1,6 +0,0 @@
-const app = require('../index');
-const { VercelResponse, VercelRequest } = require('@vercel/node');
-
-module.exports = (req = VercelRequest, res = VercelResponse) => {
-  app(req, res);
-};
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+const port = process.env.PORT || 5000; // Define the port for local testing
+
 app.use(cors());
 app.use(express.json());
 
@@ -74,16 +76,16 @@ async function run() {
 
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.send('Hello World!');
 });
 
-// Only listen on a port if not running in a Vercel serverless environment
+// Export the app as a module for Vercel serverless function
+module.exports = app;
+
+// Listen on the port when running locally
 if (process.env.NODE_ENV !== 'production') {
-  const port = process.env.PORT || 5000;
   app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
   });
 }
-
-module.exports = app;
